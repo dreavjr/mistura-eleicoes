eb08d893a6a4092c83409675c3f503a2dc9f9ddb
Eduardo Valle <valle@MacBookPro.local>
2016-04-17 22:03:46 -0300

    Corrigido e melhorado

--- mistura.py
+++ mistura.py
@@ -1,11 +1,12 @@
 from __future__ import division
 import codecs
 sim  = sorted([ l.strip().split()[-2] for l in codecs.open('sim.txt',       'rt', 'utf-8').readlines() ])
-nao  = sorted([ l.strip().split()[-2] for l in codecs.open('sim.txt',       'rt', 'utf-8').readlines() ])
+nao  = sorted([ l.strip().split()[-2] for l in codecs.open('nao.txt',       'rt', 'utf-8').readlines() ])
 abst = sorted([ l.strip().split()[-2] for l in codecs.open('abst.txt',      'rt', 'utf-8').readlines() ])
 falt = sorted([ l.strip().split()[-2] for l in codecs.open('faltantes.txt', 'rt', 'utf-8').readlines() ])
 # assume ingenuamente um prior equivalente a proporcao do total
 prior = len(sim)/(len(sim)+len(nao)+len(abst))
+print 'sim=', len(sim), 'nao=', len(nao), 'abst=', len(abst), 'prior sim=', prior
 partidos = set(sim+nao+abst+falt)
 totalSim = 0.0
 for p in partidos :
@@ -19,4 +20,4 @@ for p in partidos :
 		mistura = pFalt*(pSim/(pSim+pNao+pAbst))
 	print p, mistura
 	totalSim += mistura
-print totalSim
+print 'falt=', len(falt), 'falt sim=', totalSim
